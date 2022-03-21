241ce4c9cffe2d184b58160e080a71462f70e584
Riva Junior <rivaldo.junior@memed.com.br>
2022-03-21 18:52:45 -0300

    [IN-154] update domain

--- client/src/utils/getMemedUrl.ts
+++ client/src/utils/getMemedUrl.ts
@@ -2,7 +2,7 @@ export const getMemedURL = (api = false) => {
   const base = `${api ? 'api.' : ''}memed.com.br`;
   const AMBIENT = process.env.REACT_APP_MEMED_AMBIENT;
 
-  if (AMBIENT === 'sandbox') return `sandbox.${base}`;
+  if (AMBIENT === 'integrations') return `integrations.${base}`;
   if (AMBIENT === 'beta') return `beta.${base}`;
 
   return base;
--- server/src/shared/utils/getMemedUrl.ts
+++ server/src/shared/utils/getMemedUrl.ts
@@ -4,7 +4,7 @@ export const getMemedURL = () => {
   const base = `api.memed.com.br`;
   const AMBIENT = MEMED_AMBIENT;
 
-  if (AMBIENT === 'sandbox') return `sandbox.${base}`;
+  if (AMBIENT === 'integrations') return `integrations.${base}`;
   if (AMBIENT === 'beta') return `beta.${base}`;
 
   return base;
